e5ddc766186aa552baf154dc1b4ea7286b6df97e
machguth <horst.machguth@unifr.ch>
2023-10-05 14:20:31 +0200

    Micro changes

--- heat_flux_1D.py
+++ heat_flux_1D.py
@@ -47,7 +47,9 @@ measured_T = r'C:\horst\modeling\lateralflow\D6050043-logged_data(FS2)_v2.xlsx'
 top_thermistor_height = 2.15  # (m) height top thermistor above slab - required to correct depth intervals
 
 start_date = '2022/07/05 18:30:00'
-validation_dates = ['2022/07/05  18:30:00', '2022/08/01 00:00:00', '2022/08/17 16:00:00', '2022/08/24 00:00:00']
+validation_dates = ['2022/07/05 18:30:00', '2022/08/17 16:00:00']
+# validation_dates = ['2022/07/05 18:30:00', '2022/08/24 00:00:00']
+# '2022/08/01 00:00:00', '2022/08/24 00:00:00'
 
 days = 43  # [days] time period for which to simulate
 D = 12.  # [m] thickness of snow pack or ice slab
