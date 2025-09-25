4ea2aee77f8face2b2fb522d0d1604e328c0cc48
machguth <horst.machguth@unifr.ch>
2025-09-25 19:32:48 +0200

    Small improvements to the code.

--- heat_flux_1D_v2.py
+++ heat_flux_1D_v2.py
@@ -43,6 +43,8 @@ warnings.filterwarnings("ignore")
 time_start = datetime.datetime.now()
 
 # ============================================== input ===================================================
+# user = 'machguth'
+user = 'machg'
 
 # start and end date define the duration of the model run. The two variables are used also
 # when there is no comparison to measurements.
@@ -80,7 +82,7 @@ k_ref_i = 2.107  # [W m^-1 k^-1]
 k_ref_a = 0.024  # [W m^-1 k^-1]
 
 # output_dir = r'C:\horst\modeling\lateralflow'
-output_dir = r'C:\Users\machguth\OneDrive - Université de Fribourg\modelling\1D_heat_conduction\56d'
+output_dir = r'C:/Users/' + user + '/OneDrive - Université de Fribourg/modelling/1D_heat_conduction/56d_test'
 # output_dir = r'O:\test_1D_heat_conduction'
 # output_dir = r'D:\modelling\1d_heat_transfer'
 
